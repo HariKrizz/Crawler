c71a23cecd33be08028aacbbf4b095353e3c783e
hari <hari@hamon.in>
2022-04-05 16:22:03 +0530

    Fixed some code errors for the database-web connection

--- web_Flask.py
+++ web_Flask.py
@@ -1,22 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 import crawler
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello World</h1>"
-@app.route("/lyrics")
+    artist = crawler.get_all_artist()
+    return render_template("index.html", artists=artist )
+    # return "<h1>Hello World</h1>"
 
-def list_all_songs(artist="A"):
-    songs = crawler.get_all_songs(artist)
-    output = []
-    output.append(f"<h1>{artist}</h1>")
-    output.append("<ol>")
-    for i in songs:
-        output.append(f"<li>{i[0]}</li>")
-    output.append("</ol>")
-    return "".join(output)
+
+@app.route("/lyrics/<int:id>")
+def list_all_songs(id):
+    songs = crawler.get_all_songs(id)
+    artist= crawler.singer(id)
+    return render_template("songlist.html", artist=artist[0],songs=songs)
+
+
+@app.route("/lyric/<int:sid>")
+def lyrics(sid):
+    lyrics= crawler.get_lyrics(sid)
+    return render_template("lyrics.html", lyrics=lyrics)
 
 if __name__ == "__main__":
     app.run(debug=True)
