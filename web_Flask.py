af32ee54fb93e52e6ad1e780e86daf3f87c13901
hari <hari@hamon.in>
2022-04-06 15:44:50 +0530

    changing code parameters for viewing data in web page

--- web_Flask.py
+++ web_Flask.py
@@ -7,20 +7,24 @@ app = Flask(__name__)
 def hello():
     artist = crawler.get_all_artist()
     return render_template("index.html", artists=artist )
-    # return "<h1>Hello World</h1>"
 
 
-@app.route("/lyrics/<int:id>")
+@app.route("/songs/<int:id>")
 def list_all_songs(id):
     songs = crawler.get_all_songs(id)
-    artist= crawler.singer(id)
-    return render_template("songlist.html", artist=artist[0],songs=songs)
+    artist = crawler.singer(id)
+    artists = crawler.get_all_artist()
+    return render_template("songlist.html", artist=artist, songs=songs, artists=artists, current=id)
 
 
-@app.route("/lyric/<int:sid>")
-def lyrics(sid):
+@app.route("/songs/<int:id>/lyrics/<int:sid>")
+def lyrics(sid,id):
+    songs = crawler.get_all_songs(id)
+    artist = crawler.singer(id)
+    artists = crawler.get_all_artist()
     lyrics= crawler.get_lyrics(sid)
-    return render_template("lyrics.html", lyrics=lyrics)
+    return render_template("lyrics.html", lyrics=lyrics, songs=songs, artists=artists, artist=artist, current=sid)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
