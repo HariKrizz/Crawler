594d6ed98bab1ea09f32ed6f87edb0cac4b1260e
hari <hari@hamon.in>
2022-04-07 15:13:26 +0530

    Added the function for getting data as a list

--- static/lyrics.js
+++ static/lyrics.js
@@ -1,7 +1,18 @@
+console.log("Hello");
 
 function main() {
-    $.get({url: 'http://127.0.0.1:5000/', success: (data) => $("div.artistlist").html(data),
-});
+    $.get({url: 'http://127.0.0.1:5000/artist', success: (data) =>
+        {
+        artist_list = "";
+        data.forEach(i =>{artist_list += '<li class="list-box">' +i.name+ '</li>';});
+
+        html_tag = `<ul> ${artist_list} </ul>`;
+        $("div.artistlist").html(html_tag);
+        console.log(data);
+        },   
+    });
 }
 
 $(main);
+
+
