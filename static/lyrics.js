c76f55c1968e7780a05208ee09497fbc0bc3cc85
hari <hari@hamon.in>
2022-04-07 21:47:53 +0530

    Changed the div name as there is similar name attribute defined

--- static/lyrics.js
+++ static/lyrics.js
@@ -1,18 +1,38 @@
 console.log("Hello");
 
 function main() {
-    $.get({url: 'http://127.0.0.1:5000/artist', success: (data) =>
-        {
-        artist_list = "";
-        data.forEach(i =>{artist_list += '<li class="list-box">' +i.name+ '</li>';});
+    $.get({url: 'http://127.0.0.1:5000/artist', success: (data) => {
 
-        html_tag = `<ul> ${artist_list} </ul>`;
+        artist_list = "";
+        data.forEach(i =>{ artist_list += `<li class="artist-item" data-id=${i.id}> ${i.name} </li>`; });
+
+        html_tag = `<ol> ${artist_list} </ol>`;
         $("div.artistlist").html(html_tag);
         console.log(data);
         },   
     });
+
+    $(document).on('click', 'li.artist-item', function () {
+        artist_id = ($(this).data('id'));
+
+        $.get({url:`http://127.0.0.1:5000/songs/${artist_id}`, success: (data)=>{
+            song_list = "";
+            data.forEach(i =>{song_list += `<li class="song-item" data-id=${i.song_id}> ${i.song_name} </li>`});
+            html_tag = `<ol> ${song_list} </ol>`;
+            $("div.songlist").html(html_tag);
+            console.log(data);
+            },  
+        });
+    });
+
+    $(document).on('click', 'li.song-item', function () {
+        song_id = $(this).data('id');
+        $.get({url:`http://127.0.0.1:5000/songs/lyrics/${song_id}`, success: (data)=> {
+            html_tag = `<pre> ${data[0]} </pre>`;
+            $("div.songlyrics").html(html_tag);
+            console.log(data);
+            },  
+        });
+    }); 
 }
-
 $(main);
-
-
