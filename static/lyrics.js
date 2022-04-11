2d859bdaadb5dd59972c5f6b478e6666efde566d
Hari <hari@hamon.in>
2022-04-11 22:39:32 +0530

    changed the js code to add the api versioning for crawler

--- static/lyrics.js
+++ static/lyrics.js
@@ -1,12 +1,12 @@
 console.log("Hello");
 
 function main() {
-    $.get({url: 'http://127.0.0.1:5000/artist', success: (data) => {
+    $.get({url: 'http://127.0.0.1:5000', success: (data) => {
 
         artist_list = "";
         data.forEach(i =>{ artist_list += `<li class="artist-item" data-id=${i.id} data-name=${i.name}> ${i.name} </li>`; });
 
-        html_tag = `<ul type="none"> ${artist_list} </ol>`;
+        html_tag = `<h4 class=artist><b>Artists<b></h4><hr><ul type="none"> ${artist_list} </ul>`;
         $("div.artistlist").html(html_tag);
         console.log(data);
         },   
@@ -15,11 +15,11 @@ function main() {
     $(document).on('click', 'li.artist-item', function () {
         artist_id = ($(this).data('id'));
         artist_name = ($(this).data('name'));
-
-        $.get({url:`http://127.0.0.1:5000/songs/${artist_id}`, success: (data)=>{
+        
+        $.get({url:`http://127.0.0.1:5000/api/songs/${artist_id}`, success: (data)=>{
             song_list = "";
             data.forEach(i =>{song_list += `<li class="song-item" data-id=${i.song_id}> ${i.song_name} </li>`});
-            html_tag = `<ul type="none"> These are the songs for the artist <br> ${song_list} </ol>`;
+            html_tag = `<h4 class=songs><b>Songs<b></h4><hr><ul type="none"> ${song_list} </ol>`;
             $("div.songlist").html(html_tag);
             console.log(data);
             },  
@@ -28,8 +28,8 @@ function main() {
 
     $(document).on('click', 'li.song-item', function () {
         song_id = $(this).data('id');
-        $.get({url:`http://127.0.0.1:5000/songs/lyrics/${song_id}`, success: (data)=> {
-            html_tag = `<pre> ${data[0]} </pre>`;
+        $.get({url:`http://127.0.0.1:5000/api/songs/lyrics/${song_id}`, success: (data)=> {
+            html_tag = `<h4 class=lyric_heading><b>Lyrics<b></h4><hr><pre> ${data[0]} </pre>`;
             $("div.songlyrics").html(html_tag);
             console.log(data);
             },  
